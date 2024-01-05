8aa1d7e2a49965a8077915f3bbf30e68fa47a8ec
halrobison <halrobisoncreative@gmail.com>
2024-01-05 13:37:14 -0600

    fix: removed extra line

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -26,5 +26,4 @@ let hoursToMoon = milesToMoon / shuttleSpeed;
 let daysToMoon = hoursToMoon / 24; 
 
 // Print the results of the trip to the moon below
-
 console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon."); 
