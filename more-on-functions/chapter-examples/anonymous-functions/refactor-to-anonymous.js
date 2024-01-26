ec66088a5bf8b852dca68a4ef5d4ea097cb9cdff
halrobison <halrobisoncreative@gmail.com>
2024-01-25 21:50:43 -0600

    working on ch 10 exercises

--- more-on-functions/chapter-examples/anonymous-functions/refactor-to-anonymous.js
+++ more-on-functions/chapter-examples/anonymous-functions/refactor-to-anonymous.js
@@ -1,7 +1,11 @@
-function reverse(str) {
-  let lettersArray = str.split('');
-  let reversedLettersArray = lettersArray.reverse();
-  return reversedLettersArray.join('');
+// function reverse(str) {
+  // let lettersArray = str.split('');
+  // let reversedLettersArray = lettersArray.reverse();
+  // return reversedLettersArray.join('');
+// }
+
+let reverse = function(str) {
+  return str.split('').reverse().join('');
 }
 
 console.log(reverse("LaunchCode"));
