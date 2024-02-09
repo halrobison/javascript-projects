3d378209425bbc588746b52e1bd83aac0eb461b1
halrobison <halrobisoncreative@gmail.com>
2024-02-08 20:30:32 -0600

    completed unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,30 @@
 
 let launchcode = {
-  
+    organization: 'nonprofit', 
+    executiveDirector: 'Jeff', 
+    percentageCoolEmployees: 100,
+    programsOffered: ['Web Development', 'Data Analysis', 'LiftOff'], 
+    launchOutput: function(number){
+        let output = ''; 
+        if (number % 2 === 0) {
+            output += 'Launch'; 
+        }
+        if (number % 3 === 0) {
+            output += 'Code';
+        }
+        if (number % 5 === 0) {
+            if (output) {
+            output += ' Rocks';
+            } else {
+            output += 'Rocks';
+            }
+        } if (!output) {
+            output = "Rutabagas! That doesn't work.";
+        } else {
+            output += '!';
+        }
+        return output;
+    }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,32 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
+  test('should output Launch! with a number divisible by 2 as parameter', function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch!")
+  })
   
+  test('should output Code! with a number divisible by only 3 as parameter', function () {
+    expect(launchcode.launchOutput(3)).toBe("Code!")
+  })
+
+  test('should output Rocks! with a number divisible by only 5 as parameter', function () {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!")
+  })
+
+  test('should output LaunchCode! with a number divisible by 2 and 3', function() {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!")
+  })
+
+  test('should output Launch Rocks! with a number divisible by 2 and 5', function () {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks!")
+  })
+
+  test('should output Code Rocks! with a number divisible by 3 and 5', function () {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!")
+  })
+
+  test ('should output LaunchCode Rocks! with a number divisible by 2, 3 and 5', function() {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!")
+  })
+
 });
