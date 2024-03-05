ed86acfca76842f2905464fb35a08e0b87b453c0
halrobison <halrobisoncreative@gmail.com>
2024-03-04 20:15:46 -0600

    bonus mission: activity added

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json) {
             const container = document.getElementById("container");
+            // possible sort: const ordered = json.sort((a, b) => a.hoursInSpace > b.hoursInSpace)
             for (i=0; i < json.length; i++) {
                 console.log(json);
                 container.innerHTML += `
@@ -11,7 +12,7 @@ window.addEventListener("load", function(){
                             <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                             <li>Hours in Space: ${json[i].hoursInSpace}</li>
-                            <li>Active: ${json[i].active}</li>
+                            <li class=${Boolean(json[i].active) ? 'active' : 'inactive'}>Active: ${json[i].active}</li>
                             <li>Skills: ${json[i].skills}</li>
                             </ul>
                         </div>
