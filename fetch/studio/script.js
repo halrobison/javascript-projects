6b17f90d0debb21fbf73aa80158733ac42135e9b
halrobison <halrobisoncreative@gmail.com>
2024-03-04 20:05:29 -0600

    completed fetch studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,24 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            for (i=0; i < json.length; i++) {
+                console.log(json);
+                container.innerHTML += `
+                    <div class="astronaut">
+                        <div>
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                            <li>Hours in Space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${json[i].picture}></img>
+                    </div>
+                `;
+            }
+        })
+    })
+})
